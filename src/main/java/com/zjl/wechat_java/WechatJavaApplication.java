ca04d68ed83b2a58f675fe7125a4debeb2614135
13588330822 <13588330822@163.com>
2019-05-29 13:03:05 +0800

    first commit

---
+++ src/main/java/com/zjl/wechat_java/WechatJavaApplication.java
@@ -0,0 +1,13 @@
+package com.zjl.wechat_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WechatJavaApplication.class, args);
+	}
+
+}
